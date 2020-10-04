6618bc5767ba24fd589dae8aeffa553ca2f6228f
GoodNat <pengyul@sfu.ca>
2020-10-03 22:46:31 -0700

    return to html

--- index.php
+++ index.php
@@ -20,50 +20,45 @@
 
 </head>
 <body>
-  <?php 
-    echo "
+
     <header>
-        <div class='container'>
-          <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-              <a class='navbar-brand' href='#'>Navbar</a>
-              <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarTogglerDemo02' aria-controls='navbarTogglerDemo02' aria-expanded='false' aria-label='Toggle navigation'>
-                <span class='navbar-toggler-icon'></span>
+        <div class="container">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
               </button>
             
-              <div class='collapse navbar-collapse' id='navbarTogglerDemo02'>
-                <ul class='navbar-nav mr-auto mt-2 mt-lg-0'>
-                  <li class='nav-item active'>
-                    <a class='nav-link' href='#'>Home <span class='sr-only'>(current)</span></a>
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                   </li>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='#'>Link</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
                   </li>
-                  <li class='nav-item'>
-                    <a class='nav-link disabled' href='#' tabindex='-1' aria-disabled='true'>Disabled</a>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                   </li>
                 </ul>
-                <form class='form-inline my-2 my-lg-0'>
-                  <input class='form-control mr-sm-2' type='search' placeholder='Search'>
-                  <button class='btn btn-outline-success my-2 my-sm-0' type='submit'>Search</button>
+                <form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
               </div>
             </nav>
           </div>
-    </header>";
+    </header>
     
 
-
-
-    echo "   
     <!-- Jquery js file-->
-    <script src='js/jquery.3.5.1.js'></script>
+    <script src="js/jquery.3.5.1.js"></script>
 
     <!-- Boostrap js file-->
-    <script src='js/bootstrap.min.js'></script>
+    <script src="js/bootstrap.min.js"></script>
 
     <!--  my js file  -->
-    <script src='./js/main.js'></script>
-    ";
-  ?>
+    <script src="./js/main.js"></script>
+
 </body>
 </html>
