feb4531bab0669181e208ae48929f457ddd9d54b
DorisWang0117 <60415870+DorisWang0117@users.noreply.github.com>
2020-11-10 11:53:48 -0800

    successfully change psw and login page

---
+++ resetpswsuccessful.php
@@ -0,0 +1,153 @@
+<!--This page is designed as password successfully page-->
+<!--After user sucessfully change password, the website wil turn to this page-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<!-- Boostrap css file-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+	<!-- font awesome icon-->
+	<link rel="stylesheet" type="text/css" href="css/all.min.css">
+
+	<!-- main css -->
+	<link rel="stylesheet" type="text/css" href="css/mainstyle.css">
+</head>
+
+<body>
+	<header class="header_area">
+		<!-- code source from https://getbootstrap.com/docs/4.5/components/navbar/ -->
+		<div class="main-menu">
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<h1><a class="logo" href="index.php">ESHOP</a></h1>
+				<!--logo -->
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<div class="mr-auto"></div>
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Shop</a>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="login.php">Account</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="cart.php">Cart</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</header>
+
+
+
+	<?php
+require 'db.php'; //connection to db code
+include "auth_sessionNotActiveCheck.php";
+?>
+
+	<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title> Members Only</title>
+    </head>
+
+    <body>
+        <div class="form">
+          <p>Welcome <?php echo $_SESSION['username']; ?>!</p>
+          <p>You password is alreay reset.</p>
+          <a href="index.php">Goto home page</a>
+</div>
+    </body>
+  </html>
+
+
+	<!-- <div class="form register container mx-auto my-5">
+		<h1>Log In</h1>
+		<form action="" method="post" name="login">
+			<input type="text" name="username" placeholder="Username" required />
+			<input type="password" name="password" placeholder="Password" required />
+			<input name="submit" type="submit" value="Login" />
+		</form>
+		<p>Not registered yet? <a href='register.php'>Register Here</a></p>
+	</div> -->
+
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-6">
+					<div class="row ">
+						<div class="col-6">
+
+							<div class="text">
+								<h5>ERSHOP</h5>
+                                <ul class="list-unstyled text-small">
+                                    <li><a class="text-muted" href="index.php">Home</a></li>
+                                    <li><a class="text-muted" href="index.php">Shop</a></li>
+                                    <li><a class="text-muted" href="login.php">Account</a></li>
+                                    <li><a class="text-muted" href="cart.php">Cart</a></li>
+                                </ul>
+							</div>
+						</div>
+						<div class="col-6">
+							<div class="text">
+								<h5>Services</h5>
+								<ul class="list-unstyled text-small">
+									<li><a class="text-muted" href="login.php">Login</a></li>
+									<li><a class="text-muted" href="login.php">Register</a></li>
+									<li><a class="text-muted" href="login.php">My Cart</a></li>
+									<li><a class="text-muted" href="login.php">Checkout</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-6 text-center">
+					<div class="row ">
+						<div class="col-6">
+						</div>
+						<div class="col-6">
+							<div class="social text">
+								<h5>Contact</h5>
+								<p>604-666-8488</p>
+								<p>8888 University St</p>
+								<a href="#"><i class="fab fa-facebook"></i></a>
+								<a href="#"><i class="fab fa-instagram"></i></a>
+								<a href="#"><i class="fab fa-youtube"></i></a>
+								<a href="#"><i class="fab fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="copyrights text-center">
+				<p class="para">
+					<p>Copyright © 2018 ESHOP. All Rights Reserved. </p>
+				</p>
+			</div>
+		</div>
+
+	</footer>
+
+	</main>
+
+
+	<!-- Jquery js file-->
+	<script src="js/jquery.3.5.1.js"></script>
+
+	<!-- Boostrap js file-->
+	<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
