b331f6e5c5cd338752e6fb0f54c4eb78de4b4779
DorisWang0117 <60415870+DorisWang0117@users.noreply.github.com>
2020-12-03 05:06:36 +0800

    Add file which get users' selection

---
+++ getpromotion_selection.php
@@ -0,0 +1,13 @@
+<?php 
+    session_start();
+    require('db.php'); //connection to db code
+
+
+    /* SQL query to get results from database */
+    $promotion_val=$_GET["promotion"];
+    
+    $sql = "UPDATE users SET promotion=$promotion_val WHERE username='". $_SESSION["username"]."'";
+    echo($sql);
+    
+    $result = $conn->query($sql);
+    $conn->close();
